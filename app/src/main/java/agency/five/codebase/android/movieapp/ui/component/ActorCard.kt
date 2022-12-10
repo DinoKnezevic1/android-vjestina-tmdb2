9ee2aa3f2fe75ad649edcee878ed1aa983a273d2
DinoKnezevic1 <knezevic7p@gmail.com>
2022-12-10 17:19:17 +0100

    completed second task

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/ActorCard.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/ActorCard.kt
@@ -1,4 +1,5 @@
 package agency.five.codebase.android.movieapp.ui.component
+
 import agency.five.codebase.android.movieapp.mock.MoviesMock
 import agency.five.codebase.android.movieapp.ui.theme.GrayText
 import agency.five.codebase.android.movieapp.ui.theme.spacing
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,43 +21,43 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 data class ActorCardViewState(
-    val imageUrl:String,
+    val imageUrl: String,
     val name: String,
     val character: String
 )
 
 @Composable
-fun ActorCard (
+fun ActorCard(
     actorCardViewState: ActorCardViewState,
-    modifier:Modifier = Modifier
+    modifier: Modifier = Modifier
 ) {
-    Card {
+    Card(
+        modifier = modifier,
+        backgroundColor = Color.White
+    ) {
         Column(
-            modifier = modifier
-                .wrapContentSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start,
         ) {
             AsyncImage(
                 model = actorCardViewState.imageUrl,
                 contentDescription = "actor image",
+                contentScale = ContentScale.Crop,
                 modifier = modifier
-                    .width(125.dp)
-                    .height(170.dp)
+                    .fillMaxHeight()
             )
             Text(
                 text = actorCardViewState.name,
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = modifier
-                    .width(100.dp)
-                    .padding(start = 5.dp, end = 2.dp, top = 3.dp)
+                    .padding(MaterialTheme.spacing.extraSmall)
             )
             Text(
                 text = actorCardViewState.character,
                 fontSize = 13.sp,
                 color = GrayText,
                 modifier = modifier
-                    .width(100.dp)
+                    .padding(MaterialTheme.spacing.extraSmall)
             )
         }
         Spacer(
@@ -67,9 +69,9 @@ fun ActorCard (
 
 @Preview
 @Composable
-private fun ActorCardPreview(){
+private fun ActorCardPreview() {
     val actor = MoviesMock.getActor()
-    val actorCardViewState=ActorCardViewState(
+    val actorCardViewState = ActorCardViewState(
         actor.imageUrl.toString(),
         actor.name,
         actor.character
